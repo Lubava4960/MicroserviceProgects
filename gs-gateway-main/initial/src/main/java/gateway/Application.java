f0a76e9c789db9048326de384c967cbf32609c9b
Lubov Martyanova <lmartyanova@hse.ru>
2025-04-21 13:05:14 +0300

    add Project

---
+++ gs-gateway-main/initial/src/main/java/gateway/Application.java
@@ -0,0 +1,18 @@
+package gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+
+
+
+	}
+
+}
